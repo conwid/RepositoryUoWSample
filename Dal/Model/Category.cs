0c7e3e35ccac52973a8651a7e953913a26a05160
conwid <nagy.akos.bme.2009@gmail.com>
2019-05-15 15:22:18 +0200

    Update Category.cs

--- Dal/Model/Category.cs
+++ Dal/Model/Category.cs
@@ -4,7 +4,6 @@ namespace Dal
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
 
   
     public partial class Category
